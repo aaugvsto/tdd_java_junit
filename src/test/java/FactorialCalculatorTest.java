1f62c94de2d892bf9c1636006967910ed49b41d0
Augusto Assis <augustoassis.dev@gmail.com>
2025-05-15 22:30:38 -0300

    :sparkles: feat: TDD red step

--- src/test/java/FactorialCalculatorTest.java
+++ src/test/java/FactorialCalculatorTest.java
@@ -1,6 +1,5 @@
 import dev.augusto.FactorialCalculator;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,8 +7,7 @@ public class FactorialCalculatorTest {
 
     @Test
     public void Testa_Calculo_Fatorial_Igual_a_Zero(){
-        var result = new FactorialCalculator(0)
-                .GetResult();
+        var result = new FactorialCalculator(0).GetResult();
 
         assertEquals(1, result);
     }

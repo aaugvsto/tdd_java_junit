28030d28b25c412d401aa36d5830767732e90981
Augusto Assis <augustoassis.dev@gmail.com>
2025-05-22 11:53:37 -0300

    :test_tube: test: add test when pass fewer 8 character (failing)

--- 05 - Password Validator/src/test/java/PasswordValidatorTest.java
+++ 05 - Password Validator/src/test/java/PasswordValidatorTest.java
@@ -1,2 +1,25 @@
+import dev.augusto.PasswordValidator;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class PasswordValidatorTest {
+
+    @Test
+    public void Should_Throw_Exception_When_Password_Fewer_To_Eight_Character(){
+        //Arrange
+        String password = "123456";
+        PasswordValidator validator = new PasswordValidator(password);
+
+        try
+        {
+            // Act
+            validator.Validate();
+            fail("Expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException ex)
+        {
+            // Assert
+            assertEquals("Password must be at least 8 characters", ex.getMessage());
+        }
+    }
 }

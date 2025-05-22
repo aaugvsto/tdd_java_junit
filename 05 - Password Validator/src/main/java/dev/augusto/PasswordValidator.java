f4d15435cb03cbe526d36e8a2b5961fcf83b6a46
Augusto Assis <augustoassis.dev@gmail.com>
2025-05-22 10:43:00 -0300

    :sparkles: feat: password validator class

---
+++ 05 - Password Validator/src/main/java/dev/augusto/PasswordValidator.java
@@ -0,0 +1,23 @@
+package dev.augusto;
+
+public class PasswordValidator {
+
+    private String _password;
+
+    public PasswordValidator(String password){
+        this._password = password;
+    }
+
+    public static boolean Validate(){
+        return false;
+    }
+
+    public String GetPassword(){
+        return this._password;
+    }
+
+    public void SetPassword(String password){
+        this._password = password;
+    }
+
+}
